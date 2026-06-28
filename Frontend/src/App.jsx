14bb706ef83920bdb69001b0afd2ebbd7011d59a
utsavanand131 <utsavanand807@gmail.com>
2026-06-28 10:51:52 +0530

    add frontend setup (vite + tailwind)

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind v4 is working 🚀
+      </h1>
+    </div>
+  );
+}
