5b0cc3c29b76ed4f79481406aaf045587cc86bf2
utsavanand131 <utsavanand807@gmail.com>
2026-06-30 17:48:30 +0530

    feat: add hero section to homepage

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "../features/home/components/hero";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-row justify-between items-center px-10 w-full mt-15">
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
