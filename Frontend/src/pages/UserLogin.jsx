f9291f061156542c22a49833c9959b3295edc554
utsavanand131 <utsavanand807@gmail.com>
2026-06-30 20:15:23 +0530

    feat: add user login page integration

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import UserLoginForm from "@/features/user/components/user-login-form";
 
 const UserLogin = () => {
-  return <div>UserLogin</div>;
+  return (
+    <div className="container flex items-center justify-center min-h-screen py-8">
+      <UserLoginForm />
+    </div>
+  );
 };
 
 export default UserLogin;
