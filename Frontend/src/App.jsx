6f031ac801533c9fd58a3e043ce7d6273717d02f
utsavanand131 <utsavanand807@gmail.com>
2026-06-30 18:17:51 +0530

    feat: configure login and registration routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,8 @@ import UserRegister from "./pages/UserRegister";
 import CaptainLogin from "./pages/CaptainLogin";
 import CaptainRegister from "./pages/CaptainRegister";
 import Layout from "./features/global/components/layout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,6 +20,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/user-login" element={<UserLogin />} />
           <Route path="/user-register" element={<UserRegister />} />
           <Route path="/captain-login" element={<CaptainLogin />} />
