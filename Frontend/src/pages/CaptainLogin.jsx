e5d0689a0dc11399c3d17efabb2c134aed193396
utsavanand131 <utsavanand807@gmail.com>
2026-07-01 19:32:21 +0530

    refactor(captain): update captain login page integration

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CaptainLoginForm from "../features/captain/components/captain-login-form";
 
 const CaptainLogin = () => {
-  return <div>CaptainLogin</div>;
+  return (
+    <div className="container flex justify-center pt-24 pb-10">
+      <CaptainLoginForm />
+    </div>
+  );
 };
 
 export default CaptainLogin;
