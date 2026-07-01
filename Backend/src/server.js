5cfc5a8e8c14a48c3cb0cb4a0eb49ff4e07a00de
utsavanand131 <utsavanand807@gmail.com>
2026-07-01 17:40:26 +0530

    feat: configure user registration page and server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const server = http.createServer(app);
 
--- Frontend/src/pages/UserRegister.jsx
+++ Frontend/src/pages/UserRegister.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import UserRegisterForm from "../features/user/components/user-register-form";
 
 const UserRegister = () => {
-  return <div>UserRegister</div>;
+  return (
+    <div className="container flex items-center justify-center min-h-screen py-8">
+      <UserRegisterForm />
+    </div>
+  );
 };
 
 export default UserRegister;
