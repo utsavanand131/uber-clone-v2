64f91867801c095ee63f04defa13c21077c06030
utsavanand131 <utsavanand807@gmail.com>
2026-07-01 18:15:27 +0530

    feat: add captain registration page

--- Frontend/src/pages/CaptainRegister.jsx
+++ Frontend/src/pages/CaptainRegister.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CaptainRegisterForm from "../features/captain/components/captain-register-form";
 
 const CaptainRegister = () => {
-  return <div>CaptainRegister</div>;
+  return (
+    <div className="container flex justify-center pt-24 pb-10">
+      <CaptainRegisterForm />
+    </div>
+  );
 };
 
 export default CaptainRegister;
