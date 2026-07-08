522af28f280bbd3f19876b1b3502a64a00712946
utsavanand131 <utsavanand807@gmail.com>
2026-07-08 16:22:58 +0530

    Configure frontend for production deployment

--- Frontend/src/features/map/services/map.service.js
+++ Frontend/src/features/map/services/map.service.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL: "https://uber-clone-backend-5exd.onrender.com/api/v1",
 });
-
-// Attach JWT Token
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
@@ -16,8 +14,6 @@ API.interceptors.request.use((config) => {
   return config;
 });
 
-// Get Coordinates
-
 export const getCoordinates = async (address) => {
   const { data } = await API.get("/maps/get-coordinates", {
     params: { address },
@@ -25,8 +21,6 @@ export const getCoordinates = async (address) => {
 
   return data;
 };
-
-// Get Distance & Time
 
 export const getDistanceTime = async (origin, destination) => {
   const { data } = await API.get("/maps/get-distance-time", {
@@ -39,8 +33,6 @@ export const getDistanceTime = async (origin, destination) => {
   return data;
 };
 
-// Get Fare
-
 export const getFare = async (pickup, destination) => {
   const { data } = await API.get("/rides/get-fare", {
     params: {
@@ -52,8 +44,6 @@ export const getFare = async (pickup, destination) => {
   return data;
 };
 
-// Get Suggestions
-
 export const getSuggestions = async (input) => {
   const { data } = await API.get("/maps/get-suggestions", {
     params: {
@@ -64,8 +54,6 @@ export const getSuggestions = async (input) => {
   return data;
 };
 
-// Create Ride
-
 export const createRide = async ({ pickup, destination, vehicleType }) => {
   const { data } = await API.post("/rides/create", {
     pickup,
@@ -75,8 +63,6 @@ export const createRide = async ({ pickup, destination, vehicleType }) => {
 
   return data;
 };
-
-// Get Route
 
 export const getRoute = async (pickup, destination) => {
   const { data } = await API.get("/maps/get-route", {
@@ -89,8 +75,6 @@ export const getRoute = async (pickup, destination) => {
   return data;
 };
 
-// Confirm Ride
-
 export const confirmRide = async (rideId) => {
   const { data } = await API.post("/rides/confirm", {
     rideId,
@@ -98,8 +82,6 @@ export const confirmRide = async (rideId) => {
 
   return data;
 };
-
-// Start Ride
 
 export const startRide = async (rideId, otp) => {
   const { data } = await API.get("/rides/start-ride", {
@@ -111,8 +93,6 @@ export const startRide = async (rideId, otp) => {
 
   return data;
 };
-
-// End Ride
 
 export const endRide = async (rideId) => {
   const { data } = await API.post("/rides/end-ride", {
--- Frontend/src/socket/socket.js
+++ Frontend/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001", {
+const socket = io("https://uber-clone-backend-5exd.onrender.com", {
   transports: ["websocket"],
 });
 
